36e7ed3bb6bc1df4f057a132aa4734cbe4017fbd
Sergio Toro <sergio.toro.castano@gmail.com>
2017-11-10 19:45:30 +0100

    start bluetooth device page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
-export class AboutPage {
+export class AboutPage implements OnInit {
+  btEnabled: boolean = false;
+  btScanning: boolean = false;
+  pairedDevices = [];
+  unpairedDevices = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private bluetoothSerial: BluetoothSerial) { }
 
+  ngOnInit() {
+    this.bluetoothSerial.isEnabled()
+      .then(() => this.onBluetoothEnabled())
+      .catch(() => this.enableBluetooth())
+  }
+
+  enableBluetooth() {
+    this.bluetoothSerial.enable()
+      .then(() => this.onBluetoothEnabled())
+      .catch((error: string) => {
+        console.log('ERROR', error);
+      })
+  }
+
+  onBluetoothEnabled() {
+    this.btEnabled = true;
+    this.listPairedDevices();
+    this.discoverUnpairedDevices();
+  }
+
+  listPairedDevices() {
+    this.bluetoothSerial.list()
+      .then((devices: any): any => {
+        this.pairedDevices = devices;
+      })
+      .catch((error): void => {
+        console.log('listPairedDevices ERROR', error)
+      })
+  }
+
+  discoverUnpairedDevices() {
+    this.btScanning = true;
+    this.bluetoothSerial.discoverUnpaired()
+      .then((devices: any): any => {
+        this.btScanning = false;
+        this.unpairedDevices = devices;
+      })
+      .catch((error): void => {
+        this.btScanning = false;
+        console.log('discoverUnpairedDevices ERROR', error)
+      })
   }
 
 }
